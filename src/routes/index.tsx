a16646703599e366bd0ea8785cdd90c7663f6903
Efraim Alkhazov <efraim.alkhazov@abra-it.com>
2023-04-16 23:15:53 +0300

    feat: refactor heading

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,25 @@
+import Layout from 'components/Layout';
 import { useRoutes, Navigate, RouteObject } from 'react-router-dom';
 import { HomeScreen } from 'screens';
 
 const Routes = () => {
   const routes: RouteObject[] = [
     {
-      path: '/',
-      element: <HomeScreen />,
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <HomeScreen />,
+        },
+        {
+          path: '/works',
+          element: <div>Works</div>,
+        },
+        {
+          path: '/resume',
+          element: <div>Resume</div>,
+        },
+      ],
     },
   ];
 
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,41 +1,15 @@
 import styled from 'styled-components';
-import { Link, useNavigate } from 'react-router-dom';
-import { IoIosPaper } from 'react-icons/io';
-import { BsFileEarmarkPersonFill } from 'react-icons/bs';
-import Logo from 'assets/logo.png';
 
 import Summary from 'components/Summary';
 import Specialties from 'components/Specialties';
 
-const HomeScreen = () => {
-  const navigate = useNavigate();
-
-  return (
-    <Container>
-      <Nav>
-        <img onClick={() => navigate('/')} src={Logo} alt="logo" />
-        <Email href="mailto:efraimalkhazov@gmail.com">efraimalhazov@gmail.com</Email>
-        <LinksContainer>
-          <StyledLink to="/works">
-            <div>
-              <IoIosPaper color="#eee" />
-              Works
-            </div>
-          </StyledLink>
-          <StyledLink to="/Resume">
-            <div>
-              <BsFileEarmarkPersonFill color="#eee" />
-              Resume
-            </div>
-          </StyledLink>
-        </LinksContainer>
-      </Nav>
-      <Summary />
-      <SubTitle>What are my specialties ?</SubTitle>
-      <Specialties />
-    </Container>
-  );
-};
+const HomeScreen = () => (
+  <Container>
+    <Summary />
+    <SubTitle>What are my specialties ?</SubTitle>
+    <Specialties />
+  </Container>
+);
 
 export default HomeScreen;
 
@@ -45,61 +19,6 @@ const Container = styled.div`
   background-color: rgba(41, 41, 41, 0.9);
   padding: 40px 80px 80px 80px;
   box-shadow: rgba(0, 0, 0, 0.15) 0px 7px 29px 0px;
-`;
-
-const Nav = styled.nav`
-  display: flex;
-  color: ${({ theme }) => theme.colors.primary.light};
-  width: 100%;
-  justify-content: space-between;
-  align-items: center;
-
-  img {
-    width: 200px;
-    cursor: pointer;
-  }
-`;
-
-const LinksContainer = styled.div`
-  display: flex;
-
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-`;
-
-const StyledLink = styled(Link)`
-  font-size: ${({ theme }) => theme.typography.sizes.p};
-  transition: all 0.3s;
-  padding: 5px 10px;
-  margin: 0 2px;
-
-  div {
-    display: flex;
-    align-items: center;
-    svg {
-      margin-right: 3px;
-    }
-  }
-
-  :hover {
-    transform: rotate(-10deg) scale(1.1);
-    background-color: ${({ theme }) => theme.colors.background.red};
-  }
-`;
-
-const Email = styled.a`
-  margin: 0;
-  padding: 5px 10px;
-  transition: all 0.3s;
-  text-decoration: none;
-  color: inherit;
-
-  :hover {
-    transform: rotate(-10deg) scale(1.1);
-    background-color: ${({ theme }) => theme.colors.background.red};
-  }
 `;
 
 const SubTitle = styled.h2`
